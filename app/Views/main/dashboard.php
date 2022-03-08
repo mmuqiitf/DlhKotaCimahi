535274ec0d806192f2651c9830cb4f441ed8f05e
rendi14 <rendikuswandi042@gmail.com>
2022-03-08 22:19:54 +0700

    update

--- app/Views/main/dashboard.php
+++ app/Views/main/dashboard.php
@@ -9,36 +9,72 @@
             <!-- Left side columns -->
             <div class="col-lg-12">
                 <div class="row">
-
                     <div class="col-lg-3 col-md-4 mb-3">
-                        <div class="card info-card sales-card">
+                        <div class="card info-card">
                             <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bx bxs-user"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>Karyawan</h6>
+                                        <h5 class="card-title">123</h5>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </div><!-- End Sales Card -->
 
                     <div class="col-lg-3 col-md-4 mb-3">
-                        <div class="card info-card sales-card">
+                        <div class="card info-card">
                             <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bx bxs-location-plus"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>Titik Pantau</h6>
+                                        <h5 class="card-title">123</h5>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </div><!-- End Sales Card -->
 
                     <div class="col-lg-3 col-md-4 mb-3">
-                        <div class="card info-card sales-card">
+                        <div class="card info-card">
                             <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bx bxs-bar-chart-alt-2"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>Perusahaan</h6>
+                                        <h5 class="card-title">123</h5>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </div><!-- End Sales Card -->
 
                     <div class="col-lg-3 col-md-4 mb-3">
-                        <div class="card info-card sales-card">
+                        <div class="card info-card ">
                             <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bx bxs-layer-plus"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>Total Data BOD</h6>
+                                        <h5 class="card-title">123</h5>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </div><!-- End Sales Card -->
+
                 </div>
             </div>
+        </div>
     </section>
-
 </main><!-- End #main -->
