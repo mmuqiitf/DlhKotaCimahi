404e4aac891dc5f6f433f2a9f0c550f8938056ec
Ihsan Saiful <66325889+SaifulHD@users.noreply.github.com>
2022-03-23 20:11:34 +0700

    Update(cek list BOD)

--- app/Controllers/BODEksisting.php
+++ app/Controllers/BODEksisting.php
@@ -21,7 +21,7 @@ class BODEksisting extends BaseController
             'title' => 'BOD Eksisting',
             'bod' => $bod
         ];
-        
+
         return view('pages/BODEksisting/bodlist', $data);
     }
 
@@ -30,8 +30,48 @@ class BODEksisting extends BaseController
         //BOD Eksisting
        // $data = $this->BodEksistingModel->dataBODEksisting();
         //dd($data);
-        
-        
+        if (!$this->validate([
+            'BOD' =>[
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'BOD harus diisi'
+                ]
+            ], 'Debit' =>[
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Debit harus diisi'
+                ]
+            ], 'beban_pencemar' =>[
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Beban Pencemar harus diisi'
+                ]
+            ], 'waktu_sampling' =>[
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Waktu Sampling harus diisi'
+                ]
+            ], 'titik_pantau' =>[
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Titik Pantau harus diisi'
+                ]
+            ]
+            ]
+                ))
+        $data = [
+            'title' => 'BOD Eksisting',
+            'action' => site_url('BODEksisting/create_action'),
+            'button' => 'Create',
+            'ID_BOD_Eksisting' => set_value('ID_BOD_Eksisting'),
+            'nama_sungai' => set_value('nama_sungai'),
+            'titik_pantau' => set_value('titik_pantau'),
+            'BOD' => set_value('BOD'),
+            'Debit' => set_value('Debit'),
+            'beban_pencemar' => set_value('beban_pencemar'),
+            'waktu_sampling' => set_value('waktu_sampling'),
+        ];
+        $this->BODEksistingModel->bod_eksisting_post($data);
         return view('/pages/BODEksisting/create');
     }
 
