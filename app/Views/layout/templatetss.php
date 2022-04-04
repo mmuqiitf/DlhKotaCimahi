f8b38ec303b680352e9eba5a036ecababf5d489a
okikurniawan56 <okikurniawan213@mhs.itenas.ac.id>
2022-04-04 09:43:25 +0700

    update

---
+++ app/Views/layout/templatetss.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>DLH KOTA CIMAHI</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <?= $this->include('/source/css') ?>
+
+</head>
+
+<body>
+
+
+
+    <?= $this->include('/layout/sidebartss') ?>
+
+
+
+    <?= $this->renderSection('content') ?>
+
+
+
+
+
+    <?= $this->include('/source/js');  ?>
+
+</body>
+
+
+
+</html>
