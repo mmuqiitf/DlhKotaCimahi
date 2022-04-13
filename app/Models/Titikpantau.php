cd0c7133eba1db9c6814dfefe0b1c03da08d7211
erickgunawan <erikgunawan123321@mhs.itenas.ac.id>
2022-04-13 12:50:36 +0700

    create tss

--- app/Models/Titikpantau.php
+++ app/Models/Titikpantau.php
@@ -7,5 +7,50 @@ use CodeIgniter\Model;
 class Titikpantau extends Model
 {
     protected $table = 'titikpantau';
-    protected $allowedFields =['Param_1','Param_2','Param_3','Param_4','Param_5','Param_6','Param_7','status_mutu','Nilai_pij'];
+    protected $primaryKey = 'id_tikpan';
+    protected $allowedFields =['Param_1','Param_2','Param_3','Param_4','Param_5','Param_6','Param_7','status_mutu','Nilai_pij','periode_pantau','tanggal_pantau','id_sungai'];
+  
+
+
+    public function tss($idtss)
+    {
+        $data = $this->where([
+            'id_tikpan' => $idtss
+        ])->first();
+        return $data;
+    }
+
+    public function datatss()
+    {
+        $query = $this->db->table('titikpantau')
+            ->select('*')
+            ->get();
+        return $query->getResultArray();
+        
+    }
+
+    public function tss_post($data)
+    {
+        return $this->db->table('titikpantau')->insert($data);
+    }
+
+    
+
+    public function getta9()
+    {
+        $builder = $this->db->table('titikpantau');
+        $builder->select('*');
+        return $builder->get();
+    }
+
+
+    public function getAll()
+    {
+        return $this->db->get($this->_table)->result();
+    }
+
+    public function getById($id)
+    {
+        return $this->db->get_where($this->_table, ["id_tikpan" => $id])->row();
+    }
 }
