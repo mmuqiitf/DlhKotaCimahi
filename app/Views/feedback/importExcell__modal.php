c75aff26fcb2b301b2492f28dff7b33035aec733
mrayandika15 <mrayandika15@gmail.com>
2022-04-24 13:21:04 +0700

    feat(add button, add template file, modify style)

--- app/Views/feedback/importExcell__modal.php
+++ app/Views/feedback/importExcell__modal.php
@@ -6,13 +6,27 @@
         <div class="modal-content custom__modal">
             <div class="modal-body custom__modal__content custom__modal__content__excell ">
 
-                <input class="form-control <?= ($validation->hasError('file_excel')) ? 'is-invalid' : ''; ?>" name="file_excel" type="file" accept=".xlsx, .xls">
+
+                <div class="alert alert-danger" role="alert">
+                    Jika ingin menggunakan fitur ini silahkan download template excel dibawah ini.
+                </div>
+
+                <a href="/template_excell/BODEksisting.xlsx" download="Template Input BOD Eksisting">
+                    <button class="button__excell button__download__template" type="button">Download Template</button>
+                </a>
+
+
+
+                <input class="form-control <?= ($validation->hasError('file_excel')) ? 'is-invalid' : ''; ?>"
+                    name="file_excel" type="file" accept=".xlsx, .xls">
                 <small class="text-danger justify-content-end">
                     <?= $validation->getError('file_excel'); ?>
                 </small>
                 <div class="button__container__modal">
-                    <button class="button__excell"><i class="bi bi-check-lg icon__confirm" type="submit"></i>Import Now</button>
-                    <button type="button" class="button__excell" data-bs-dismiss="modal"><i class="bi bi-x-lg icon__dismiss"></i>Tidak</button>
+                    <button class="button__excell"><i class="bi bi-check-lg icon__confirm" type="submit"></i>Import
+                        Now</button>
+                    <button type="button" class="button__excell" data-bs-dismiss="modal"><i
+                            class="bi bi-x-lg icon__dismiss"></i>Tidak</button>
                 </div>
             </div>
         </div>
